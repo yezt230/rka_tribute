662e583378e17841c48d275e84170c02aac3d480
yezt230 <mjollnirsandwich@gmail.com>
2025-12-21 19:19:01 -0600

    Jumping frames for spritesheet, rudimentary animation controls (need to add state machine)

--- rka-tribute/scenes/player.gd
+++ rka-tribute/scenes/player.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -1000.0
+const GRAV_ADJUSTMENT: float = 2.0
+
 @onready var player_dir: float = 1.0
 @onready var player_sprite = $Sprite2D
 @onready var animation_player = $AnimationPlayer
@@ -10,7 +11,7 @@ const JUMP_VELOCITY = -400.0
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * GRAV_ADJUSTMENT * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("UP") and is_on_floor():
@@ -19,12 +20,26 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("LEFT", "RIGHT")
-	get_orientation(direction)
+	get_orientation(direction)	
+
 	if direction:
 		velocity.x = direction * SPEED
 		animation_player.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation_player.play("idle")
+
+	#animation handling section
+	if velocity.y != 0:
+		if velocity.y < -400:
+			player_sprite.frame = 11
+		elif velocity.y > 400:
+			player_sprite.frame = 9
+		else:
+			player_sprite.frame = 10
+	elif direction:
+		animation_player.play("run")
+	else:
 		animation_player.play("idle")
 
 	move_and_slide()
