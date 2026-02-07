9a0a304d86e8d6ef9d9bc083765f5bf20efe80d6
yezt230 <mjollnirsandwich@gmail.com>
2026-02-07 17:47:37 -0600

    Changing orb attack to Area2D for hit detection

--- rka-tribute/scenes/boss_orb_attack.gd
+++ rka-tribute/scenes/boss_orb_attack.gd
@@ -1,27 +1,30 @@
-extends CharacterBody2D
+extends Area2D
 
-@onready var timer = $Timer
+@onready var timer: Timer = $Timer
 
-const GRAV_ADJUSTMENT: float = 2.0
+# Tuneable constants
+#@export var gravity: float = 1200.0
+@export var initial_y_velocity: float = -1000.0
+@export var grav_adjustment: float = 2.0
+
+var velocity: Vector2
 var history: Array[float] = []
 
-func _ready():
-	var initial_velocity = Vector2(get_next_velocity_x(),-1000)
-	velocity = initial_velocity
+func _ready() -> void:
+	velocity = Vector2(get_next_velocity_x(), initial_y_velocity)
 	timer.timeout.connect(on_timer_timeout)
-	
-	print(get_next_velocity_x())
-	
 
 func _physics_process(delta: float) -> void:
-	velocity.y += (get_gravity().y * GRAV_ADJUSTMENT * delta)
-	move_and_slide()
+	# Apply gravity
+	velocity.y += gravity * grav_adjustment * delta
 	
+	# Integrate position
+	global_position += velocity * delta
 
-func on_timer_timeout():
+func on_timer_timeout() -> void:
 	queue_free()
-
-
+	
+	
 func weighted_random_away(
 		min_value: float,
 		max_value: float,
@@ -33,40 +36,28 @@ func weighted_random_away(
     strength < 1.0  = softer bias
 	"""
 
-	# No history → pure random
-	if history.size() == 0:
+	if history.is_empty():
 		return randf_range(min_value, max_value)
 
-	# One value → push away from it
 	var center := history[-1]
-
-	# Two values → push away from their midpoint
 	if history.size() >= 2:
 		center = (history[-1] + history[-2]) * 0.5
 
-	# Normalize center to 0–1
 	var center_t := inverse_lerp(min_value, max_value, center)
-
-	# Sample uniformly in 0–1
 	var t := randf()
 
-	# Distance from center (0–1)
 	var dist: float = abs(t - center_t)
-
-	# Apply repulsion curve
 	var weight := pow(dist, strength)
 
-	# Randomly choose direction away from center
 	if t < center_t:
 		t = clamp(center_t - weight, 0.0, 1.0)
 	else:
 		t = clamp(center_t + weight, 0.0, 1.0)
 
 	return lerp(min_value, max_value, t)
-	
-	
+
 func get_next_velocity_x() -> float:
-	var value = weighted_random_away(
+	var value := weighted_random_away(
 		400.0,
 		1000.0,
 		history,
---
+++ rka-tribute/scenes/hurt.gd
@@ -0,0 +1,16 @@
+extends State
+
+@export var idle_state: State
+@export var jump_state: State
+@export var run_state: State
+
+func enter() -> void:
+	super()
+	parent.animation_player.play("idle")
+
+func process_input(_event: InputEvent) -> State:
+	if Input.is_action_just_pressed("UP"):
+		return jump_state
+	elif Input.is_action_just_pressed("LEFT") or Input.is_action_just_pressed("RIGHT"):
+		return run_state
+	return null
