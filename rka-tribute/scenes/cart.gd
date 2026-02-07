aaee5977df9b91e8ac1e3e63c7be1a3ee30208d3
yezt230 <mjollnirsandwich@gmail.com>
2026-02-07 12:41:06 -0600

    Cart tracks to player's horizontal position

--- rka-tribute/scenes/cart.gd
+++ rka-tribute/scenes/cart.gd
@@ -1,9 +1,20 @@
 extends CharacterBody2D
 
+@onready var player = $"../Player"
+
+func _ready():
+	print("player")
+	print(player)
+	
+
+#func _process(delta: float) -> void:
+	#print("player.x: " + str(player.position.x))
+
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += get_gravity().y * 2.0 * delta		
 	else:
 		velocity.y = 0
-		
+	
+	global_position.x = player.position.x	
 	move_and_slide()
