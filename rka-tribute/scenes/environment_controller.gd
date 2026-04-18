d2251ba2c87f59a1fdee8aa53ac490b958feacad
yezt230 <mjollnirsandwich@gmail.com>
2026-04-18 14:03:15 -0500

    More track in the rubbing section progress

--- rka-tribute/scenes/environment_controller.gd
+++ rka-tribute/scenes/environment_controller.gd
@@ -6,6 +6,8 @@ extends Node
 @onready var wood_scroller = $"../BG/WoodScroller"
 @onready var cart = $"../Cart"
 @onready var cart_animation_player = $"../Cart/WheelSprite/AnimationPlayer"
+@onready var track = $"../Track"
+@onready var track_animation_player = $"../Track/AnimationPlayer"
 
 var current_direction: float = 0.0
 var is_active: bool = true
@@ -32,6 +34,7 @@ func _physics_process(_delta: float) -> void:
 
 	wood_scroller.velocity.x = velocity_x
 	cart.velocity.x = velocity_x
+	track.velocity.x = velocity_x
 
 # --- CUTSCENE OVERRIDE ---
 func start_cart_cutscene(velocity_x: float):
@@ -46,7 +49,11 @@ func stop_cart_cutscene():
 func apply_override_motion():
 	wood_scroller.velocity.x = override_velocity
 	cart.velocity.x = override_velocity
+	track.velocity.x = override_velocity
+	#track_animation_player.play("track_loop")
 
 func stop():
 	wood_scroller.velocity.x = 0
 	cart.velocity.x = 0
+	track.velocity.x = 0
+	
