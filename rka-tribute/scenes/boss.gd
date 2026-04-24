02cde9e03aab61b39afcca70e60ad2861f7f6908
yezt230 <mjollnirsandwich@gmail.com>
2026-04-24 13:57:17 -0500

    Health component for boss

--- rka-tribute/scenes/boss.gd
+++ rka-tribute/scenes/boss.gd
@@ -5,17 +5,18 @@ extends CharacterBody2D
 @onready var state_machine = $StateMachine
 @onready var state_label = $StateLabel
 @onready var speed_label = $SpeedLabel
+@onready var health_component = $HealthComponent
 
 func _ready():
 	state_machine.init(self)
-
+	health_component.zero_health.connect(_on_zero_health)
 
 func _physics_process(delta: float) -> void:
 	var current_state = state_machine.current_state
 	if current_state:
 		state_label.text = current_state.name
-	#speed_label.text = str(global_position.x)
-	speed_label.text = str("%0.1f" % global_position.x)
+	#speed_label.text = str("%0.1f" % global_position.x)
+	speed_label.text = str(health_component.health)
 	# Add the gravity. 
 	if not is_on_floor():
 		velocity.y += get_gravity().y * 2.0 * delta
@@ -27,3 +28,7 @@ func _physics_process(delta: float) -> void:
 	coupling_rod_sprite.rotation = -coupling_rod_orbit.rotation
 		
 	move_and_slide()
+
+
+func _on_zero_health():
+	queue_free()
---
+++ rka-tribute/scenes/health_component.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+signal zero_health
+
+@onready var boss = $".."
+
+var starting_health = 100
+var health = starting_health
+
+func _on_boss_hurtbox_body_entered(body):
+	health -= 5
+	
+	if health <= 0:
+		health = 0
+		emit_signal("zero_health")
+	
+	
