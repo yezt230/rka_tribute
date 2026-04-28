a7f96a5e5efb04a266e2b9ac7f4671bc27fbf4fa
yezt230 <mjollnirsandwich@gmail.com>
2026-04-28 18:52:26 -0500

    Elevated track on action section

--- rka-tribute/track_from_single.gd
+++ rka-tribute/track_from_single.gd
@@ -2,21 +2,22 @@ extends Node2D
 
 @onready var track_from_single = $"."
 @export var track_single : PackedScene
-enum Mode { MAIN, RUBBING }
-@export var mode: Mode = Mode.MAIN
+enum Mode { MAIN, RUBBING, SECOND }
+@export var mode: Mode
 @onready var track_single_width : float
 @onready var level_layer = get_tree().get_first_node_in_group("level")
 
 @onready var activated_yet : bool = false
-@onready var amount_of_tracks_to_spawn : int = 30
+@onready var amount_of_tracks_to_spawn : int = 24
 @onready var track_single_spwan_incrementer : int = 0
 @onready var spawn_timer = $SpawnTimer
 
 func _ready():
-	#match mode:
-		#Mode.MAIN:
-	if mode == Mode.MAIN:
-		main_track_animation()
+	match mode:
+		Mode.MAIN:
+			main_track_animation()
+		Mode.SECOND:
+			activate_spawning()
 
 
 func activate_spawning():
@@ -24,12 +25,6 @@ func activate_spawning():
 	#activated_yet = true
 	track_single_spwan_incrementer = 0
 	spawn_timer.start()
-		
-
-func _process(_delta):
-	if track_single_spwan_incrementer > amount_of_tracks_to_spawn:
-		if spawn_timer:
-			spawn_timer.stop()
 			
 			
 func main_track_animation():
@@ -52,11 +47,14 @@ func main_track_animation():
 # @todo this keeps spawning unlimited new tracks even after
 # enough looping ones are already onscreen
 func _on_spawn_timer_timeout():
-	track_single_spwan_incrementer += 1
-	print("spawn inc: " + str(track_single_spwan_incrementer))
-	var track_single_instance = track_single.instantiate()
-	var ts_anim = track_single_instance.get_node("TrackSingle/AnimationPlayer")
-	if ts_anim:
-		ts_anim.play("track_single_loop")
-	add_child(track_single_instance)
-	track_single_instance.position = Vector2(-500, 0)
+	if track_single_spwan_incrementer < amount_of_tracks_to_spawn:
+		track_single_spwan_incrementer += 1
+		print("spawn inc: " + str(track_single_spwan_incrementer))
+		var track_single_instance = track_single.instantiate()
+		var ts_anim = track_single_instance.get_node("TrackSingle/AnimationPlayer")
+		if ts_anim:
+			ts_anim.play("track_single_loop")
+		add_child(track_single_instance)
+		track_single_instance.position = Vector2(-500, 0)
+	else:
+		spawn_timer.stop()
