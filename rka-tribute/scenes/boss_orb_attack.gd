4de887bfe73d2a50029bc7f8bc03a846dc926176
yezt230 <mjollnirsandwich@gmail.com>
2026-05-10 17:58:18 -0500

    Weighted randomness of boss orb attack--now cannot launch as far when nearer the right side of the screen

--- rka-tribute/scenes/boss_orb_attack.gd
+++ rka-tribute/scenes/boss_orb_attack.gd
@@ -1,19 +1,26 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
-
+@onready var boss = get_tree().get_first_node_in_group("boss")
 # Tuneable constants
 #@export var gravity: float = 1200.0
 @export var initial_y_velocity: float = -1000.0
 @export var grav_adjustment: float = 2.0
 
+@export var left_velocity_min := 400.0
+@export var left_velocity_max := 1000.0
+
+@export var right_velocity_min := 280.0
+@export var right_velocity_max := 350.0
+
 var velocity: Vector2
 var history: Array[float] = []
 
 func _ready() -> void:
-	#DEBUG
-	#velocity = Vector2(get_next_velocity_x(), initial_y_velocity)
-	velocity = Vector2(500, initial_y_velocity)
+	#DEBUG: orb attack initial velocity
+	velocity = Vector2(get_next_velocity_x(), initial_y_velocity)
+	print("boss orb vel: " + str(velocity))
+	#velocity = Vector2(500, initial_y_velocity)
 	timer.timeout.connect(on_timer_timeout)
 
 
@@ -34,20 +41,17 @@ func weighted_random_away(
 		max_value: float,
 		history: Array[float],
 		strength: float = 2.0) -> float:
-	"""
-    strength > 1.0  = stronger repulsion
-    strength = 1.0  = linear
-    strength < 1.0  = softer bias
-	"""
 
 	if history.is_empty():
 		return randf_range(min_value, max_value)
 
 	var center := history[-1]
+
 	if history.size() >= 2:
 		center = (history[-1] + history[-2]) * 0.5
 
 	var center_t := inverse_lerp(min_value, max_value, center)
+
 	var t := randf()
 
 	var dist: float = abs(t - center_t)
@@ -61,15 +65,46 @@ func weighted_random_away(
 	return lerp(min_value, max_value, t)
 
 
+
 func get_next_velocity_x() -> float:
+	# Get viewport width
+	var screen_width := get_viewport_rect().size.x
+
+	# Convert boss X position into 0.0 -> 1.0
+	var boss_t: float = clamp(
+		boss.global_position.x / screen_width,
+		0.0,
+		1.0
+	)
+
+	# As boss moves right:
+	# velocity ranges become smaller
+	var min_vel: float = lerp(
+		left_velocity_min,
+		right_velocity_min,
+		boss_t
+	)
+
+	var max_vel: float = lerp(
+		left_velocity_max,
+		right_velocity_max,
+		boss_t
+	)
+
+	if boss.dir == 1:
+		min_vel = max(min_vel, 400.0)
+
+	max_vel = max(max_vel, min_vel)
+
 	var value := weighted_random_away(
-		400.0,
-		1000.0,
+		min_vel,
+		max_vel,
 		history,
 		2.5
 	)
 
 	history.append(value)
+
 	if history.size() > 2:
 		history.pop_front()
 
