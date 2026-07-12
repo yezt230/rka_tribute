2ac83519078dfebef601861f64f2a442d0740c5a
yezt230 <mjollnirsandwich@gmail.com>
2026-07-12 12:34:37 -0500

    Code trigger to play roof tearing off animation

--- rka-tribute/scenes/defeated.gd
+++ rka-tribute/scenes/defeated.gd
@@ -3,12 +3,18 @@ extends State
 @onready var phase_transition_timer = $"../../PhaseTransitionTimer"
 @onready var defeat_explosion_particle = $"../../DefeatExplosionParticle"
 @onready var explosion_stream_player = $"../../ExplosionStreamPlayer"
+@onready var cover_animation_player = $"../../CoverAnimationPlayer"
+@onready var cover_tear_off_animation_player = $"../../CoverTearOffAnimationPlayer"
 
 func enter():
 	defeat_explosion_particle.emitting = true
 	parent.phase += 1
 	phase_transition_timer.start()
 	play_explosion_sound_loop()
+#	cover tearing off animation
+	if parent.phase == 2:
+		cover_animation_player.play("tear_off")
+		cover_tear_off_animation_player.play("tear_off")	
 	#removing claw hitboxes when boss is defeated in Phase 2
 	if parent.phase == 4:
 		var claws = get_tree().get_nodes_in_group("claws")
