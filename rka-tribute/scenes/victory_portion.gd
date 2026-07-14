65b600741c6850b09e1c0c5b7cc7a14e69c48b9c
yezt230 <mjollnirsandwich@gmail.com>
2026-07-13 22:26:32 -0500

    correct iris in anim

--- rka-tribute/scenes/victory_portion.gd
+++ rka-tribute/scenes/victory_portion.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
-@onready var iris_in = $IrisIn
+@onready var iris_in = $CanvasLayer2/IrisIn
 
-func _ready():
-	#func play_iris_in() -> void:
+
+func _on_iris_in_timer_timeout():
+	iris_in_animation()
+	
+	
+func iris_in_animation():
 	var material2 := iris_in.material as ShaderMaterial
 	material2.set_shader_parameter("progress", 0.0)
 
